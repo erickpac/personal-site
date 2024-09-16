34a86f9c4eea4df6d5c618310d30945acd0ff8b3
Erick Pac <erickj.pac@gmail.com>
2024-09-16 16:48:07 -0600

    chore: add profile component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
 import { colors } from "@/constants";
-import { mockJobs, mockPeople } from "@/mocks/mocks";
-import { Github, Linkedin } from "lucide-react";
+import { mockJobs, mockCoworkers, mockTechnologies } from "@/mocks/mocks";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
-import { Button } from "@/components/ui/button";
 import { CoworkerCard } from "./components/coworker-card";
 import { JobCard } from "./components/job-card";
-import "./App.css";
+import { TechCard } from "./components/tech-card";
+import { Profile } from "@/components/profile";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -27,59 +26,7 @@ function App() {
           <section
             className={`mb-12 ${isLoaded ? "animate-fade-in animate-delay-2" : "opacity-0"}`}
           >
-            <h2 className="mb-4 text-3xl font-bold">Profile</h2>
-            <div className={`${colors.subtle} overflow-hidden rounded-lg`}>
-              <div className="p-6">
-                <div className="flex flex-col items-center gap-6 md:flex-row">
-                  <img
-                    src="https://media.licdn.com/dms/image/v2/C5603AQGgHgXFXeev1g/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1658981393786?e=1732147200&v=beta&t=TNWeP17OT0vqMtZ72HYIorxtOd8MAHQKU5_8JttXLTA"
-                    alt="Profile Picture"
-                    width={200}
-                    height={200}
-                    className="rounded-full transition-transform duration-300 hover:scale-105"
-                  />
-
-                  <div>
-                    <p className="mb-4">
-                      Hi, I'm John Doe, a passionate software engineer with 5
-                      years of experience in building scalable web applications.
-                      I specialize in JavaScript, React, and Node.js, and I'm
-                      always eager to learn new technologies.
-                    </p>
-                    <div className="flex gap-4">
-                      <a
-                        href="https://github.com/erickpac"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <Button
-                          variant="outline"
-                          size="icon"
-                          className={`${colors.subtle} ${colors.subtleHover} text-gray-700 transition-all duration-300 hover:scale-105`}
-                        >
-                          <Github className="h-5 w-5" />
-                          <span className="sr-only">GitHub</span>
-                        </Button>
-                      </a>
-                      <a
-                        href="https://linkedin.com/in/erick-pac"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <Button
-                          variant="outline"
-                          size="icon"
-                          className={`${colors.subtle} ${colors.subtleHover} text-gray-700 transition-all duration-300 hover:scale-105`}
-                        >
-                          <Linkedin className="h-5 w-5" />
-                          <span className="sr-only">LinkedIn</span>
-                        </Button>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Profile colors={colors} />
           </section>
 
           <section
@@ -87,25 +34,8 @@ function App() {
           >
             <h2 className="mb-4 text-3xl font-bold">Tech Stack</h2>
             <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-6">
-              {[
-                "JavaScript",
-                "TypeScript",
-                "React",
-                "Node.js",
-                "Python",
-                "Docker",
-              ].map((tech) => (
-                <div
-                  key={tech}
-                  className={`rounded-lg p-4 ${colors.subtle} ${colors.subtleHover} flex transform items-center justify-center transition-all duration-300 hover:scale-105`}
-                >
-                  <img
-                    src={`/placeholder.svg?height=64&width=64&text=${tech}`}
-                    alt={tech}
-                    width={64}
-                    height={64}
-                  />
-                </div>
+              {mockTechnologies.map((tech, index) => (
+                <TechCard key={index} colors={colors} tech={tech} />
               ))}
             </div>
           </section>
@@ -126,8 +56,8 @@ function App() {
           >
             <h2 className="mb-4 text-3xl font-bold">People I've Worked With</h2>
             <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-              {mockPeople.map((person, index) => (
-                <CoworkerCard key={index} colors={colors} person={person} />
+              {mockCoworkers.map((coworker, index) => (
+                <CoworkerCard key={index} colors={colors} coworker={coworker} />
               ))}
             </div>
           </section>
---
+++ src/components/profile.tsx
@@ -0,0 +1,75 @@
+import { Colors } from "@/types/types";
+import { Button } from "@/components/ui/button";
+import { Github, Linkedin } from "lucide-react";
+
+type ProfileProps = {
+  colors: Colors;
+};
+
+export const Profile = ({ colors }: ProfileProps) => {
+  return (
+    <>
+      <h2 className="mb-4 text-3xl font-bold">Profile</h2>
+      <div className={`${colors.subtle} overflow-hidden rounded-lg`}>
+        <div className="p-6">
+          <div className="flex flex-col items-center gap-6 md:flex-row">
+            <img
+              src="https://media.licdn.com/dms/image/v2/C5603AQGgHgXFXeev1g/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1658981393786?e=1732147200&v=beta&t=TNWeP17OT0vqMtZ72HYIorxtOd8MAHQKU5_8JttXLTA"
+              alt="Profile Picture"
+              width={200}
+              height={200}
+              className="rounded-full transition-transform duration-300 hover:scale-105"
+            />
+
+            <div>
+              <p className="mb-4">
+                Hi, I'm Erick Pac, highly skilled software engineer with over 8
+                years of experience in developing and optimizing a wide range of
+                software applications. Recognized for being diligent, possessing
+                strong critical thinking abilities, and excelling in
+                communication. Continuously enhances technical skills and
+                knowledge through proactive learning, demonstrating a dedication
+                to personal and professional growth. Proficient in collaborating
+                effectively within teams, consistently contributing to project
+                success with a blend of deep technical expertise and strong
+                interpersonal skills. Experienced in all phases of the software
+                development lifecycle, including design, coding, testing, and
+                deployment across multiple platforms and technologies.
+              </p>
+              <div className="flex gap-4">
+                <a
+                  href="https://github.com/erickpac"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className={`${colors.subtle} ${colors.subtleHover} text-gray-700 transition-all duration-300 hover:scale-105`}
+                  >
+                    <Github className="h-5 w-5" />
+                    <span className="sr-only">GitHub</span>
+                  </Button>
+                </a>
+                <a
+                  href="https://linkedin.com/in/erick-pac"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className={`${colors.subtle} ${colors.subtleHover} text-gray-700 transition-all duration-300 hover:scale-105`}
+                  >
+                    <Linkedin className="h-5 w-5" />
+                    <span className="sr-only">LinkedIn</span>
+                  </Button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
